b3521978c8135573c07574152038026090cbc062
Myles Lewis <mglewis@bu.edu>
2018-12-10 16:15:24 -0500

    Connecting everything

--- app/src/main/java/com/wordpress/mentalhealthmonitor/teambestteamfinal/HomeScreen.java
+++ app/src/main/java/com/wordpress/mentalhealthmonitor/teambestteamfinal/HomeScreen.java
@@ -1,7 +1,9 @@
 package com.wordpress.mentalhealthmonitor.teambestteamfinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -23,11 +25,43 @@ public class HomeScreen extends AppCompatActivity {
         toHelp = (ImageButton) findViewById(R.id.helpButton);
         toData = (ImageButton) findViewById(R.id.dataButton);
 
+        toJournal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openJournal();
+            }
+        });
 
+        toHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHelp();
+            }
+        });
+
+        toData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openData();
+            }
+        });
 
         nameFromMain = (TextView) findViewById(R.id.textView);
         userNameString = GetName.editTextString;
         nameFromMain.setText(userNameString);
 
     }
+    public void openData(){
+        Intent intent = new Intent(this, Data.class);
+        startActivity(intent);
+    }
+    public void openJournal(){
+        Intent intent = new Intent(this, Journal.class);
+        startActivity(intent);
+    }
+    public void openHelp(){
+        Intent intent = new Intent(this, Help.class);
+        startActivity(intent);
+    }
+
 }
